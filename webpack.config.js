8bebfc49ec9ad15c54d624cd43b9cb16bb9209e6
qs <543649375@qq.com>
2017-03-23 17:36:24 +0800

    同步

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ var TransferWebpackPlugin = require('transfer-webpack-plugin'); //拷贝文件
 var prod = process.env.NODE_ENV === 'production' ? true : false;
 
 //需要用到glob模块 
-var glob = require('glob');
+var glob = require('glob'),
+    pageArr = [];
 var getEntry = function() {
     var entry = {};
     //首先我们先读取我们的开发目录
@@ -16,14 +17,15 @@ var getEntry = function() {
         var n = name.slice(name.lastIndexOf('javascript/') + 11, name.length - 2);
         n = n.slice(0, n.lastIndexOf('/')); //接着我对路径字符串进行了一些裁剪成想要的路径
         entry[n] = name;
+        pageArr.push(n);
     });
-    entry['vendors'] = ['jquery'];//增加jquery
-    console.log(entry);
+    entry['vendors'] = ['jquery']; //增加jquery
+    console.log('entry', entry);
     return entry;
 };
 
 module.exports = {
-    entry: getEntry(),//入口
+    entry: getEntry(), //入口
     output: {
         path: path.join(__dirname, "build"),
         //path: __dirname,
@@ -42,7 +44,7 @@ module.exports = {
             loader: 'rgl'
         }, {
             test: /\.js$/,
-            loader: 'babel?cacheDirectory=false'
+            loader: 'babel?cacheDirectory=true'
         }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract("style-loader", "css-loader")
@@ -54,31 +56,6 @@ module.exports = {
         //把入口文件里面的数组打包成verdors.js
         //new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
         new ExtractTextPlugin("[name].css?[hash:10]"), //合成css文件 对应不同的html模板
-        new HtmlwebpackPlugin({//登陆页面
-            template: path.resolve(__dirname, 'template/login.html'),
-            filename: 'login.html', //filename配置的html文件目录是相对于webpackConfig.output.path路径而言的，不是相对于当前项目目录结构的。
-            //chunks这个参数告诉插件要引用entry里面的哪几个入口
-            chunks: ['vendors', 'login'],
-            //要把script插入到标签里
-            inject: 'body'
-        }),
-        //注册页面
-        new HtmlwebpackPlugin({
-            template: path.resolve(__dirname, 'template/register.html'),
-            filename: 'register.html', //filename配置的html文件目录是相对于webpackConfig.output.path路径而言的，不是相对于当前项目目录结构的。
-            //chunks这个参数告诉插件要引用entry里面的哪几个入口
-            chunks: ['vendors', 'register'],
-            //要把script插入到标签里
-            inject: 'body'
-        }),
-        new HtmlwebpackPlugin({
-            template: path.resolve(__dirname, 'template/index.html'),
-            filename: 'index.html', //filename配置的html文件目录是相对于webpackConfig.output.path路径而言的，不是相对于当前项目目录结构的。
-            //chunks这个参数告诉插件要引用entry里面的哪几个入口
-            chunks: ['vendors', 'main'],
-            //要把script插入到标签里
-            inject: 'body'
-        }),
         new CleanWebpackPlugin(['build'], {
             root: __dirname,
             verbose: true,
@@ -91,6 +68,16 @@ module.exports = {
         }], path.resolve(__dirname, "build"))
     ]
 };
+//根据enter创建同名html
+pageArr.forEach(function(page) {
+    console.info('$page页面', page,__dirname)
+    var htmlPlugin = new HtmlwebpackPlugin({
+        filename: page + '.html',
+        template: path.resolve(__dirname, 'template/' + page + '.html'),
+        chunks: [page, 'vendors']
+    });
+    module.exports.plugins.push(htmlPlugin);
+});
 // 判断开发环境还是生产环境,添加uglify等插件
 if (process.env.NODE_ENV === 'production') {
     module.exports.plugins = (module.exports.plugins || [])
@@ -99,13 +86,13 @@ if (process.env.NODE_ENV === 'production') {
                     NODE_ENV: '"production"'
                 }
             }),
-            /*new webpack.optimize.UglifyJsPlugin({
+            new webpack.optimize.UglifyJsPlugin({
                 compress: {
                     warnings: false,
                     drop_debugger: true,
                     drop_console: true
                 }
-            }),*/
+            }),
             new webpack.optimize.OccurenceOrderPlugin() //用了很多hashing，确保有一个一致的模块顺序
         ]);
 } else {
@@ -119,7 +106,7 @@ if (process.env.NODE_ENV === 'production') {
         watchOptions: {
             aggregateTimeout: 300,
             poll: 1000
-          },
+        },
         stats: {
             colors: true
         },
@@ -130,7 +117,7 @@ if (process.env.NODE_ENV === 'production') {
     module.exports.plugins = (module.exports.plugins || [])
         .concat([
             new OpenBrowserPlugin({
-                url: 'http://localhost:8090/login.html'
+                url: 'http://localhost:8090'
             }),
             new webpack.optimize.OccurenceOrderPlugin() //用了很多hashing，确保有一个一致的模块顺序
         ]);
